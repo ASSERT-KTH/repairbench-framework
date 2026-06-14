e98b448b16abe23fb85b74d1ad74232da4745de3
Tom Sorger <tomsorger314@gmail.com>
2026-06-14 10:37:49 +0200

    fix setup.sh

--- setup.sh
+++ setup.sh
@@ -12,7 +12,7 @@ docker pull maven:3.9.8-eclipse-temurin-8;
 cd benchmarks/defects4j;
 cpanm --installdeps .;
 ./init.sh;
-cd ../..;
+cd ../..
 
 ### GitBug-Java
 cd benchmarks/gitbug-java;
@@ -22,9 +22,12 @@ poetry install --no-root;
 if [ -z "$CI" ]; then
  poetry run ./gitbug-java setup;
 fi
+cd ../..
 
 ### BugsInPy
 docker build -t bugsinpy -f Dockerfile.bugsinpy .
+# Cleanup old container if it exists
+docker rm -f bugsinpy-container 2>/dev/null || true
 # Start the container and keep it running
 docker run -d --name bugsinpy-container -it bugsinpy tail -f /dev/null
 # Initialize container
